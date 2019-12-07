8a27f039d7957f37d9111674a7cc120f61a8cbe8
Moriarty221B <munesum@ymail.com>
2019-12-07 18:30:01 +0200

    Updates

---
+++ precache-manifest.3cea1619286542445e4478bd109c50a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a327cab7fb7df7226bb8eda8f74b72",
+    "url": "/todolistdeploy/index.html"
+  },
+  {
+    "revision": "b4b83e1715a44ea8fbec",
+    "url": "/todolistdeploy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ffa2ac7ac1833f420dd0",
+    "url": "/todolistdeploy/static/js/2.64c9d278.chunk.js"
+  },
+  {
+    "revision": "b4b83e1715a44ea8fbec",
+    "url": "/todolistdeploy/static/js/main.0dba1537.chunk.js"
+  },
+  {
+    "revision": "6ffce34bfbea402de689",
+    "url": "/todolistdeploy/static/js/runtime-main.8eb5b04b.js"
+  }
+]);
